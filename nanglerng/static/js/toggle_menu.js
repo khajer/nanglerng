8211641c06a66a41cbe67a7a704b9114d59d0e8c
kha <itsaraK>
2022-02-23 21:40:32 +0700

    add other template file

---
+++ nanglerng/static/js/toggle_menu.js
@@ -0,0 +1,7 @@
+function openSubmenu(){
+    if(document.getElementById("sub-menu-title").style.display === "inline"){
+        document.getElementById("sub-menu-title").style.display = "none";
+    }else{
+        document.getElementById("sub-menu-title").style.display = "inline";
+    }
+}
