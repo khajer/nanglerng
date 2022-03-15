cbc84b1baead1849ee46371615e704a3f20e4aba
kha <itsaraK>
2022-03-15 23:11:18 +0700

    change scp to rsync

--- cp_to_dev.sh
+++ cp_to_dev.sh
@@ -1,5 +1,5 @@
 #media
-scp -r ubuntu@ec2-54-179-108-80.ap-southeast-1.compute.amazonaws.com:~/nanglerng/media ./nanglerng
+rsync -r ubuntu@ec2-54-179-108-80.ap-southeast-1.compute.amazonaws.com:~/nanglerng/media ./nanglerng
 #database (sqlite3)
-scp ubuntu@ec2-54-179-108-80.ap-southeast-1.compute.amazonaws.com:~/nanglerng/db.sqlite3 ./nanglerng/db.sqlite3
+rsync ubuntu@ec2-54-179-108-80.ap-southeast-1.compute.amazonaws.com:~/nanglerng/db.sqlite3 ./nanglerng/db.sqlite3
 
