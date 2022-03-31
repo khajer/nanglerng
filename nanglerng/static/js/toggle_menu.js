dbb21b6768da09b0fb9d001ae877e73c1b5f7c83
kha <itsaraK>
2022-03-31 17:30:25 +0700

    add logo menu

--- nanglerng/static/js/toggle_menu.js
+++ nanglerng/static/js/toggle_menu.js
@@ -5,3 +5,15 @@ function openSubmenu(){
         document.getElementById("sub-menu-title").style.display = "inline";
     }
 }
+
+function showLinkPopup(){    
+    if(document.getElementById("mini-menu").style.display === "inline"){        
+        document.getElementById("mini-menu").style.display = "none";
+    }else{        
+        document.getElementById("mini-menu").style.display = "inline";
+    }
+}
+
+function hidePopupMiniMenu(){
+    document.getElementById("mini-menu").style.display = "none";
+}
