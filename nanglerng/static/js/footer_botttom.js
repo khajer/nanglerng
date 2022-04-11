1550db406c69c7f1af7d67c662b928ca6c79078e
kha <itsaraK>
2022-04-12 02:31:00 +0700

    udpate footer bottom

--- nanglerng/static/js/footer_botttom.js
+++ nanglerng/static/js/footer_botttom.js
@@ -1,8 +1,4 @@
-function resizeContentMain(){
-    // let height = window.innerHeight; 
-    console.log("5555");
-
-    
-    document.getElementById("main-content").style.height = "1200px"; ;
+function resizeContentMain(){    
+    document.getElementById("main-content").style.height = "1000px"; ;
     
 }
