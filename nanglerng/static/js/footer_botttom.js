9c47b3ff003e7132a7a7b715eeb11be7c1cc5f64
itsara <itsara@itsaras-MacBook-Pro.local>
2022-04-12 22:05:22 +0700

    update footer to bottom

--- nanglerng/static/js/footer_botttom.js
+++ nanglerng/static/js/footer_botttom.js
@@ -1,4 +1,21 @@
 function resizeContentMain(){    
-    document.getElementById("main-content").style.height = "1000px"; ;
+    
+    let h = document.getElementById("normal-header").clientHeight;    
+    let m = document.getElementById("main-content").clientHeight;
+    let b = document.getElementById("footer").clientHeight;
+    
+    let height = window.innerHeight;
+
+    console.log(h); // 66.5
+    console.log(m);
+    console.log(b); // 64    
+    console.log(height);
+
+    let difScrll = height - (h+m+b);
+    if(difScrll > 0){
+        console.log("xxx");
+        console.log(difScrll);
+        document.getElementById("main-content").style.height = (m+difScrll)+"px";
+    }
     
 }
