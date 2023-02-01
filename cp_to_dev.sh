c5157d50225e5fbab8a47a650d00a54625d29f90
itsara <itsara@kha-macbookpro-M2.local>
2023-02-01 14:44:26 +0700

    change file name

--- cp_to_dev.sh
+++
@@ -1,6 +0,0 @@
-#media
-rsync -r ubuntu@ec2-54-251-220-210.ap-southeast-1.compute.amazonaws.com:~/nanglerng/media ./nanglerng
-#database (sqlite3)
-rsync ubuntu@ec2-54-251-220-210.ap-southeast-1.compute.amazonaws.com:~/nanglerng/db.sqlite3 ./nanglerng/db.sqlite3
-
-54.251.220.210
