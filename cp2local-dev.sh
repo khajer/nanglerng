9ee83b3369f0b67c9c250644388de17eee6a7e84
itsara <itsara@kha-macbookpro-M2.local>
2023-02-03 22:38:08 +0700

    change public ip

--- cp2local-dev.sh
+++ cp2local-dev.sh
@@ -1,6 +1,6 @@
 #media
-rsync -r ubuntu@ec2-54-251-220-210.ap-southeast-1.compute.amazonaws.com:~/nanglerng/media ./nanglerng
+rsync -r ubuntu@ec2-52-74-12-59.ap-southeast-1.compute.amazonaws.com:~/nanglerng/media ./nanglerng
 #database (sqlite3)
-rsync ubuntu@ec2-54-251-220-210.ap-southeast-1.compute.amazonaws.com:~/nanglerng/db.sqlite3 ./nanglerng/db.sqlite3
+rsync ubuntu@ec2-52-74-12-59.ap-southeast-1.compute.amazonaws.com:~/nanglerng/db.sqlite3 ./nanglerng/db.sqlite3
 
-54.251.220.210
+
